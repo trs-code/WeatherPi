0ae70614a4dfa579ce65054e49fe530d1f482251
trs-code <trs252@cornell.edu>
2026-01-27 16:28:04 -0500

    Changes

--- operate.c
+++ operate.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "src/include/cml.h"
 #include "src/include/sensor/sensor.h"
 
 int main()
 {
+    float vals[3] = {0.0, 0.0, 0.0};
     int success = sensor_setup();
+    //model = load_model("weathrModelContextBest");
+    if(success != 0) exit(EXIT_FAILURE);
+
+    success = getWeatherInfo(vals);
+    if(success != 0) exit(EXIT_FAILURE);
+
+    for(int i = 0; i < 3; i++) printf("%d\n", vals[i]);
 
     return 0;
 }
