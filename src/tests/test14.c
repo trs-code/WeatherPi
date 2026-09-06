040a2ebd9c983fb281484c64db3e220752834ca8
trs-code <trs252@cornell.edu>
2026-09-06 17:56:47 -0400

    Added attention functionality - IMPORTANT: THIS IS NOT THE SELF ATTENTION FUNCTIONALITY IN A TRANSFORMER ARCHITECTURE

---
+++ src/tests/test14.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include "../include/cml.h"
+
+/*  
+Model Structure
+                    
+            X(t - 1)            h(t)
+        inLayer----hiddenLayer0 ---  outLayer
+                        |
+                        |h(t-1)
+                        |        
+    prevLayers --- hiddenLayer1 
+                        .
+                        .
+                        .
+                2 * context x windowSize
+*/
+
+int main() 
+{
+    layer** windowLayers = NULL;
+    int windowSize = 10;
+
+    layer* inLayer0 = make_input_layer(3);
+    if(inLayer0 == NULL)
+    {
+        printf("Memory allocation failed at inLayer\n");
+        goto error1;
+    }
+    
+    layer* hiddenLayer0 = make_hidden_layer((layer**[]){&inLayer0}, 1, 1, 'h');
+    if(hiddenLayer0 == NULL)
+    {
+        printf("Memory allocation failed at hiddenLayer0\n");
+        goto error2;
+    }
+
+    extend_context(hiddenLayer0, windowSize, (&windowLayers));
+    if(windowLayers == NULL) goto error3;
+
+    layer* attnLayer0 = make_attention_layer((layer**[]){&hiddenLayer0}, 1, 1, windowSize, 'x');
+    attnLayer0->layerType = 'a';
+
+    layer* outLayer = make_output_layer((layer**[]){&attnLayer0}, 1, 1, 'g');
+    if(outLayer == NULL)
+    {
+        printf("Memory allocation failed at outLayer\n");
+        goto error3;
+    }
+
+    model *myModel = construct_model((layer**[]){&inLayer0}, &outLayer, 4 + (2 * windowSize), 1, 1.0f, 'q');
+    if(myModel == NULL)
+    {
+        printf("Memory allocation failed at model\n");
+        goto error4;
+    }
+
+    printf("Model creation successful\n\n");
+
+    memcpy((*myModel->inLayers[0])->outputs, (float[]){0.05, 0.10, 0.15}, 3*sizeof(float));
+    memcpy(myModel->targets, (float[]){0.905405}, sizeof(float));
+
+    forward_out(myModel, 0.0);
+    sgd_backprop(myModel);
+
+    for(int i = 0; i < 1; i++) printf("hiddenLayer0 output[%d] is: %f\n", i, hiddenLayer0->outputs[i]);
+
+    printf("\nModel output is: %f\n", (*myModel->outLayer)->outputs[0]);
+    printf("\noutLayer Backerror is: %f\n", outLayer->backErrors[0]);
+    
+    calculate_and_apply_grads(myModel);
+    
+    for(int i = 0; i < 1500; i++)
+    {
+        shift_model(myModel, 't');
+        forward_out(myModel, 0.0);
+        sgd_backprop(myModel);
+        calculate_and_apply_grads(myModel);
+    }
+
+    printf("\nModel output is: %f\tTarget is : %f", (*myModel->outLayer)->outputs[0], myModel->targets[0]);
+    printf("\noutLayer Weights:\n");
+    for(int i = 0; i < 1; i++) printf("[%f]\n", outLayer->weights[0][i]);
+
+    printf("\noutLayer Backerror is: %f\n", outLayer->backErrors[0]);
+    for(int i = 0; i < 1; i++) printf("hiddenLayer0 Backerror[%d] is: %f\n", i, hiddenLayer0->backErrors[i]);
+
+    printf("\noutLayer preActivation is: %f\n", outLayer->preActivations[0]);
+    for(int i = 0; i < 1; i++) printf("hiddenLayer0 preActivation[%d] is: %f\n", i, hiddenLayer0->preActivations[i]);
+
+    //save_model(myModel, "test14Model.cml");
+
+    hakai_model(&myModel);
+
+    free(windowLayers);
+    windowLayers = NULL;
+    
+    printf("Test Successful\n");
+    return 0;
+
+error4:
+    hakai_layer(&outLayer);
+error3:
+    hakai_layer(&hiddenLayer0);
+error2:
+    hakai_layer(&inLayer0);
+error1:
+    exit(EXIT_FAILURE);
+}
